0e2c28fb55057d0fc13045b7eae21950e38c089e
MunJaeyoung <1234aiden@naver.com>
2025-01-01 15:07:39 +0900

    chore: re 표현식 수정

--- instaclone/app/common/utils.py
+++ instaclone/app/common/utils.py
@@ -3,20 +3,20 @@ from typing import Optional
 
 import re
 
-EMAIL_PATTERN = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-PHONE_NUMBER_PATTERN = r'^010\d{8}$'
-USERNAME_PATTERN = r'^[a-zA-Z0-9._]{3,30}$'
+EMAIL_PATTERN = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+PHONE_NUMBER_PATTERN = re.compile(r'^010\d{8}$')
+USERNAME_PATTERN = re.compile(r'^[a-zA-Z0-9._]{3,30}$')
 
 def identify_input_type(input_str: str) -> Optional[str]:
     """
     주어진 문자열이 email, phone_number, username중 어떤 것인지 확인하고 반환합니다.
     """
 
-    if re.match(EMAIL_PATTERN, input_str):
+    if EMAIL_PATTERN.match(input_str):
         return "email"
-    elif re.match(PHONE_NUMBER_PATTERN, input_str):
+    elif PHONE_NUMBER_PATTERN.match(input_str):
         return "phone_number"
-    elif re.match(USERNAME_PATTERN, input_str):
+    elif USERNAME_PATTERN.match(input_str):
         return "username"
     else:
         return None
