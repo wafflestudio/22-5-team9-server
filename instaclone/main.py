5dbf64efe1878c9e78f98e930a662ee253e132d1
MunJaeyoung <1234aiden@naver.com>
2025-01-05 23:44:19 +0900

    fix: cors error

--- instaclone/main.py
+++ instaclone/main.py
@@ -7,6 +7,19 @@ app = FastAPI()
 
 app.include_router(api_router, prefix="/api")
 
+origins = [
+    "https://d3l72zsyuz0duc.cloudfront.net/",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 @app.get("/test")
 def test_api():
     return "SUCCESS"
