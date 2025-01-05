4d6e759d93d56542eec24ba1f8f356a319d1b74c
MunJaeyoung <1234aiden@naver.com>
2025-01-05 23:43:18 +0900

    test: test api 추가

--- instaclone/main.py
+++ instaclone/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from instaclone.api import api_router
 
 app = FastAPI()
 
 app.include_router(api_router, prefix="/api")
+
+@app.get("/test")
+def test_api():
+    return "SUCCESS"
