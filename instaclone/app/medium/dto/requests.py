f8ea660550e2cf91396c3bd1774ddcb2e14f3679
Sunghwan Choi <bchoi9852@gmail.com>
2025-01-06 17:17:58 +0900

    Add validator to medium/requests.py
    Added validator to medium/requests.py so that user has to input at least one of each (post or story id)

--- instaclone/app/medium/dto/requests.py
+++ instaclone/app/medium/dto/requests.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationError, root_validator
 from fastapi import File, Form, UploadFile
 
 
@@ -6,6 +6,15 @@ class MediumUploadRequest(BaseModel):
     post_id: int | None = Form(None)
     story_id: int | None = Form(None)
     file: UploadFile = File(...)
+
+    @root_validator
+    def validate_medium(cls, values):
+        post_id = values.get("post_id")
+        story_id = values.get("story_id")
+
+        if not post_id and not story_id:
+            raise ValueError("You must provide at least one of post_id or story_id.")
+        return values
 
     class Config:
         schema_extra = {
