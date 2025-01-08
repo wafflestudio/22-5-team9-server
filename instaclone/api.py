99e31989a7f80d1ab33861110bdea65aaa11bc2e
MunJaeyoung <1234aiden@naver.com>
2025-01-08 15:56:07 +0900

    feat: story router 추가

--- instaclone/api.py
+++ instaclone/api.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 # from instaclone.app.follower.views import follower_router
 # from instaclone.app.medium.views import medium_router
 from instaclone.app.post.views import post_router
-# from instaclone.app.story.views import story_router
+from instaclone.app.story.views import story_router
 from instaclone.app.user.views import user_router
 
 api_router = APIRouter()
@@ -13,5 +13,5 @@ api_router = APIRouter()
 # api_router.include_router(follower_router, prefix="/follower", tags=["follower"])
 # api_router.include_router(medium_router, prefix="/medium", tags=["medium"])
 api_router.include_router(post_router, prefix="/post", tags=["post"])
-# api_router.include_router(story_router, prefix="/story", tags=["story"])
+api_router.include_router(story_router, prefix="/story", tags=["story"])
 api_router.include_router(user_router, prefix="/user", tags=["user"])
