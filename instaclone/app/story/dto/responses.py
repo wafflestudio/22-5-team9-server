f4d63dd215f36437e4d7fdd61ab6cf8a5b3d4b47
MunJaeyoung <1234aiden@naver.com>
2025-01-08 15:16:06 +0900

    feat: response 파일 경로 추가

--- instaclone/app/story/dto/responses.py
+++ instaclone/app/story/dto/responses.py
@@ -1,3 +1,4 @@
+from typing import List
 from pydantic import BaseModel
 from datetime import datetime
 
@@ -8,12 +9,15 @@ class StoryDetailResponse(BaseModel):
     creation_date: datetime
     expiration_date: datetime
     user_id: int
+    file_url: List[str]
     
     @staticmethod
     def from_story(story: Story) -> "StoryDetailResponse":
+        file_urls = [m.url for m in story.media]
         return StoryDetailResponse(
             story_id=story.story_id,
             creation_date=story.creation_date,
             expiration_date=story.expiration_date,
-            user_id=story.user_id
+            user_id=story.user_id,
+            file_url=file_urls
         )
