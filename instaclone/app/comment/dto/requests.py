84dfe5e926144805b4f213cae192592db9b7ab78
MunJaeyoung <1234aiden@naver.com>
2025-01-11 23:45:55 +0900

    feat: comments dto 추가

--- instaclone/app/comment/dto/requests.py
+++ instaclone/app/comment/dto/requests.py
@@ -1,13 +1,9 @@
-# from typing import Annotated
-# from pydantic import AfterValidator, BaseModel
+from pydantic import BaseModel
 
-# from instaclone.common.errors import InvalidFieldFormatError
+class CommentCreateRequest(BaseModel):
+    comment_text: str
+    post_id: int
+    parent_id: int
 
-# # Validation definitions
-# def validateComment(text: str | None) -> str | None:
-#     return
-
-# class CommentCreateRequest(BaseModel):
-#     comment_text: Annotated[str, AfterValidator(validateComment)]
-#     post_id: int
-#     parent_id: int
+class CommentEditRequest(BaseModel):
+    comment_text: str
--- instaclone/app/comment/dto/responses.py
+++ instaclone/app/comment/dto/responses.py
@@ -1,36 +1,26 @@
-# from typing import Self
-# from pydantic import BaseModel
+from typing import List
+from pydantic import BaseModel
 
-# from instaclone.app.comment.models import Comment
-# from instaclone.app.user.models import User
-# from instaclone.app.post.models import Post
+from instaclone.app.comment.models import Comment
+from instaclone.app.user.models import User
+from instaclone.app.post.models import Post
 
 
-# class CommentDetailResponse(BaseModel):
-#     comment_id: int
-#     user_id: int
-#     post_id: int
-#     parent: int | None
-#     comment_text: str
-#     replies: list[int]
+class CommentDetailResponse(BaseModel):
+    comment_id: int
+    user_id: int
+    post_id: int
+    parent_id: int | None
+    comment_text: str
+    replies: List[int]
 
-#     @staticmethod
-#     def from_comment(comment: Comment) -> "CommentDetailResponse":
-#         if comment.parent:
-#             parent = comment.parent
-#             parent_id = parent.comment_id
-#         else:
-#             parent_id = None
-#         reply_ids = []
-#         if comment.replies:
-#             for reply in comment.replies:
-#                 reply_ids.append(reply.comment_id)
-
-#         return CommentDetailResponse(
-#             comment_id=comment.comment_id,
-#             user_id=comment.user_id,
-#             post_id=comment.post_id,
-#             parent=parent_id,
-#             comment_text=comment.comment_text,
-#             replies=reply_ids
-#         )
+    @staticmethod
+    async def from_comment(comment: Comment) -> "CommentDetailResponse":
+        return CommentDetailResponse(
+            comment_id=comment.comment_id,
+            user_id=comment.user_id,
+            post_id=comment.post_id,
+            parent_id=comment.parent_id,
+            comment_text=comment.comment_text,
+            replies=[re.comment_id for re in comment.replies]
+        )
