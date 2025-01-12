a50fc33d19f3f45a48eca9c14504a8c22cd3ade3
MunJaeyoung <1234aiden@naver.com>
2025-01-12 15:30:29 +0900

    feat: comment dto 수정

--- instaclone/app/comment/dto/requests.py
+++ instaclone/app/comment/dto/requests.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class CommentCreateRequest(BaseModel):
     comment_text: str
     post_id: int
-    parent_id: int
+    parent_id: int | None = None
 
 class CommentEditRequest(BaseModel):
     comment_text: str
--- instaclone/app/comment/dto/responses.py
+++ instaclone/app/comment/dto/responses.py
@@ -1,10 +1,11 @@
 from typing import List
 from pydantic import BaseModel
+from sqlalchemy.sql import select
 
 from instaclone.app.comment.models import Comment
 from instaclone.app.user.models import User
 from instaclone.app.post.models import Post
-
+from instaclone.database.annotation import SESSION
 
 class CommentDetailResponse(BaseModel):
     comment_id: int
@@ -12,15 +13,26 @@ class CommentDetailResponse(BaseModel):
     post_id: int
     parent_id: int | None
     comment_text: str
-    replies: List[int]
 
     @staticmethod
-    async def from_comment(comment: Comment) -> "CommentDetailResponse":
+    def from_comment(comment: Comment) -> "CommentDetailResponse":
         return CommentDetailResponse(
             comment_id=comment.comment_id,
             user_id=comment.user_id,
             post_id=comment.post_id,
             parent_id=comment.parent_id,
             comment_text=comment.comment_text,
-            replies=[re.comment_id for re in comment.replies]
+        )
+    
+class ReplyDetailResponse(BaseModel):
+    comment_id: int
+    user_id: int
+    comment_text: str
+
+    @staticmethod
+    def from_reply(reply: Comment) -> "ReplyDetailResponse":
+        return ReplyDetailResponse(
+            comment_id=reply.comment_id,
+            user_id=reply.user_id,
+            comment_text=reply.comment_text
         )
